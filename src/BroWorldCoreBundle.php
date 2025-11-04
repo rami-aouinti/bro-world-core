159188a0dd53d556d3cb05f4074a0fbc2485c841
rami aouinti <rami.aouinti@gmail.com>
2025-11-04 23:35:57 +0100

    create bundle core broworld

--- src/BroWorldCoreBundle.php
+++ src/BroWorldCoreBundle.php
@@ -21,9 +21,15 @@ final class BroWorldCoreBundle extends AbstractBundle
             ->arrayNode('security')
             ->addDefaultsIfNotSet()
             ->children()
-            // valeur par défaut sûre; tu peux mettre null si tu préfères forcer la config app
             ->scalarNode('secured_path_regex')->defaultValue('^/api/(?!.*(security)|(test)|(doc)).*$')->end()
             ->end()
+            ->end()
+            ->children()
+            ->arrayNode('messenger')->addDefaultsIfNotSet()->children()
+            ->arrayNode('failed_retry')->addDefaultsIfNotSet()->children()
+            ->booleanNode('is_retryable')->defaultFalse()->end()
+            ->integerNode('waiting_time')->min(0)->defaultValue(0)->end()
+            ->end()->end()
             ->end()
             ->end();
     }
@@ -40,5 +46,7 @@ final class BroWorldCoreBundle extends AbstractBundle
             'bro_world_core.security.secured_path_regex',
             $config['security']['secured_path_regex'] ?? '^/api/(?!.*(security)|(test)|(doc)).*$'
         );
+        $container->parameters()->set('bro_world_core.messenger.failed_retry.is_retryable', $config['messenger']['failed_retry']['is_retryable'] ?? false);
+        $container->parameters()->set('bro_world_core.messenger.failed_retry.waiting_time', $config['messenger']['failed_retry']['waiting_time'] ?? 0);
     }
 }
--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -6,6 +6,7 @@ use Bro\WorldCoreBundle\Infrastructure\Service\LexikJwtAuthenticatorService;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Bro\WorldCoreBundle\Transport\EventSubscriber\ExceptionSubscriber;
+use Bro\WorldCoreBundle\Infrastructure\Messenger\Strategy\FailedRetry;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 
@@ -27,4 +28,7 @@ return static function (ContainerConfigurator $container): void {
         ->arg('$environment', '%kernel.environment%');
     $services->set(LexikJwtAuthenticatorService::class)
         ->arg('$path', '%bro_world_core.security.secured_path_regex%');
+    $services->set(FailedRetry::class)
+        ->arg('$isRetryable', '%bro_world_core.messenger.failed_retry.is_retryable%')
+        ->arg('$retryWaitingTime', '%bro_world_core.messenger.failed_retry.waiting_time%');
 };
